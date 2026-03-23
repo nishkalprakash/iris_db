f0dacdb72f125ef05ec4b75dde7248ed04be7529
nishkalprakash <nishkalprakash@gmail.com>
2026-03-23 15:13:44 +0530

    trying to add iris_syn

--- commands.js
+++ commands.js
@@ -2,9 +2,22 @@
 db.getCollection("meta").find(
   {},
   {
-    "orig": 0,
-    "mask_irisseg": 0,
+    orig: 0,
+    mask_irisseg: 0,
     "norm_def.stats.num_eyes_per_person": 0,
     "norm_def.stats.num_samples_per_eye": 0,
+  }
+);
+
+//db.getCollection("IITD_v1").find({})
+db.getCollection("IITD_v1").updateMany(
+  {}, // empty filter to update all documents
+  {
+    $rename: {
+      "norm_def.orig_rel_path": "norm_def.orig_rel_",
+      "mask_irisseg.orig_rel_path": "mask_irisseg.orig_rel_",
+      "seg_irisseg.orig_rel_path": "seg_irisseg.orig_rel_",
+      "overlay_irisseg.orig_rel_path": "overlay_irisseg.orig_rel_",
+    },
   }
 );
